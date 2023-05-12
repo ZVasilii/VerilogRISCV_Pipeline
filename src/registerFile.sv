c7fa5ff74c629840b6f24451e9e6392112a54f88
ZVasilii <zcvasilii@gmail.com>
2023-05-12 03:33:15 +0300

    UPD registerFile

--- src/registerFile.sv
+++ src/registerFile.sv
@@ -1,24 +1,28 @@
-module RGF(
+//RISC-V register file module
+
+module registerFile (
     input logic  clk,
     input logic we,
-    input logic[4:0] ReadN,
-    input logic[4:0] ReadN,
+    input logic[4:0] ReadN1,
+    input logic[4:0] ReadN2,
     input logic[4:0] WriteN,
     input logic[31:0] In,
     output logic[31:0] Out1,
     output logic[31:0] Out2
 );
 
-logic [31:0] registers [31:0] /*verilator public*/;
+logic [31:0] registerFile [31:0];
 
-// read registers
-assign val1 = (rn1 == 0) ? 0 : registers[rn1];
-assign val2 = (rn2 == 0) ? 0 : registers[rn2];
+//Combinational reading from regFile
+assign Out1 = (ReadN1 === 4'b0) ? 4'b0 : registerFile[ReadN1];
+assign Out2 = (ReadN2 === 4'b0) ? 4'b0 : registerFile[ReadN2];
 
-// write to register wn
-always @(negedge clk) begin
-    if(we && wn !=0) begin
-        registers[wn] <= data;
+//Sequential writing on Negedge (to solve issue with writeback)
+always_ff @(negedge clk)
+begin
+    if(we && (WriteN != 4'b0))
+    begin
+        registerFile[WriteN] <= In;
     end
 end
 
