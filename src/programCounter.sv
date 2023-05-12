1ddf6efa99e76544526c493c3860b1b7d3682d19
ZVasilii <zcvasilii@gmail.com>
2023-05-12 06:24:04 +0300

    Modified PC

--- src/programCounter.sv
+++ src/programCounter.sv
@@ -1,21 +1,27 @@
 `include "src/constants.sv"
-
+//Verified
 module programCounter(
-    input clk,
-    input en,
-    input[1:0] PCMux,
-    input[31:0] PC_Execute,
-    input[31:0] Imm,
-    input[31:0] Reg1,
+    input logic clk,
+    input logic en,
+    input logic [1:0] PCMux,
+    input logic [31:0] PC_Execute,
+    input logic [31:0] Imm,
+    input logic [31:0] Reg1,
 
-    output[31:0] PCOut
+    output logic [31:0] PCOut
 );
+
+assign PCOut = pc;
+
+//PC = PC + 4
+//OR PC = PC_Execute + imm
+//OR PC = Reg + imm
 logic [31:0] pc /*verilator public*/;
-logic [31:0] PC_SRC1 = (NextPC === `PC_IF) ? pc : ((NextPC === `PC_EX) ? PC_Execute : Reg1);
-logic [31:0] PC_SRC2 = (NextPC === `PC_REG) ? Imm : 4;
-always @(posedge clk) begin
+wire [31:0] PC_SRC1 = (PCMux === `PC_IF) ? pc : ((PCMux === `PC_EX) ? PC_Execute : Reg1);
+wire [31:0] PC_SRC2 = (PCMux === `PC_REG || PCMux === `PC_EX) ? Imm : 32'h4;
+always_ff @(posedge clk) begin
     if (en) begin
-        PCOut <= PC_SRC1 + PC_SRC2;
+        pc <= PC_SRC1 + PC_SRC2;
     end
 end
 
